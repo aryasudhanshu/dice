fe3f0f46e75533510ccf88fc3f12aca04b97fe98
Sudhanshu Arya <aryasudhanshu@gmail.com>
2019-06-06 10:46:50 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Die from './Die';
+import RollDice from './RollDice';
 import './App.css';
 import './Die.css';
 
 function App() {
   return (
     <div className="App">
-      <Die face="five"/>
+      <RollDice />
     </div>
   );
 }
